53e28e496ad234867df30ae39f13b9724e700dcb
microlinx2019 <52910104+microlinx2019@users.noreply.github.com>
2019-08-02 15:27:33 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excese_Bank__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            Console.Write("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("After swapping, the first number is: " + num1);
+            Console.WriteLine("the second number is: " + num2);
+            Console.ReadLine();
+        }
+    }
+}
